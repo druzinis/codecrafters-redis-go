26d9b473d047a8fc83df3f6745dc818a1f76f73c
Druzinis <andrius.druzinis@gmail.com>
2022-12-22 22:21:47 +0100

    read same connection

--- app/server.go
+++ app/server.go
@@ -14,14 +14,16 @@ func main() {
 	 if err != nil {
 	 	fmt.Println("Failed to bind to port 6379")
 	 	os.Exit(1)
-	 }
+     }
+     var conn net.Conn
+     conn, err = l.Accept()
+     if err != nil {
+         fmt.Println("Error accepting connection: ", err.Error())
+         os.Exit(1)
+     }
+     var buffer = make([]byte, 100);
      for {
-         var conn net.Conn
-         conn, err = l.Accept()
-         if err != nil {
-             fmt.Println("Error accepting connection: ", err.Error())
-             os.Exit(1)
-         }
-         go conn.Write([]byte("+PONG\r\n"))
+         conn.Read(buffer)
+         conn.Write([]byte("+PONG\r\n"))
      }
 }
