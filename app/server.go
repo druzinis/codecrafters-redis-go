e3ef2965657c7748cacd314bedb7d59bacd9976a
Druzinis <andrius.druzinis@gmail.com>
2022-12-29 16:11:24 +0100

    attempt at ECHO command

--- app/server.go
+++ app/server.go
@@ -3,10 +3,25 @@ package main
 import (
 	"fmt"
 	 "net"
-	 "os"
+    "os"
+    "regexp"
 )
 
+// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
+var regex = regexp.MustCompile(`^\*2\r\n\$\d+\r\n([A-Z]+)\r\n\$\d+\r\n(.*)\r\n$`)
+
 func main() {
+//    fmt.Println("start")
+//    var match = regexp.MustCompile(`^\*2\r\n\$(\d+)\r\n`).FindSubmatch([]byte("*2\r\n$456\r\n"))
+//    fmt.Println(match[1])
+//    fmt.Println("end")
+//    fmt.Println("start")
+//    match = regex.FindSubmatch([]byte("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"))
+//    fmt.Println(match[1])
+//    fmt.Println(match[2])
+//
+//    fmt.Println("end")
+//    fmt.Println(regex.FindAllStringSubmatch("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n", 0))
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
@@ -22,14 +37,34 @@ func main() {
             fmt.Println("Error accepting connection: ", err.Error())
             os.Exit(1)
         }
-        go handleConne(conn)
+        go handleConn(conn)
     }
 }
 
-func handleConne(conn net.Conn) {
+func handleConn(conn net.Conn) {
     var buffer = make([]byte, 100);
     for {
-        conn.Read(buffer)
-        conn.Write([]byte("+PONG\r\n"))
+        var byteCount, err = conn.Read(buffer)
+        if err != nil {
+            fmt.Println("Error reading from connection: ", err.Error())
+            os.Exit(1)
+        }
+//        var string = string(buffer[:byteCount])
+        var val = regex.FindSubmatch(buffer[:byteCount])[2]
+        var send = append([]byte("+"), val...)
+        send = append(send, []byte("\r\n")...)
+        conn.Write(send)
+//        conn.Write([]byte("+PONG\r\n"))
     }
 }
+
+//struct RedisCommand {
+//    type RedisCommandType
+//}
+//
+//type RedisCommandType int
+//
+//const (
+//    PING RedisCommandType = []byte('PING')
+//    ECHO = []byte('ECHO')
+//)
